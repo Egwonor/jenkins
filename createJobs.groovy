6b8ad455dff54bf5ded258390bad75a10abf82a8
Egwonor <45492800+Egwonor@users.noreply.github.com>
2020-08-05 16:56:52 +0200

    Update createJobs.groovy

--- createJobs.groovy
+++ createJobs.groovy
@@ -12,7 +12,7 @@ pipelineJob('book-inventory-job') {
             scm {
                 git {
                     remote {
-                        url 'https://github.com/tkgregory/spring-boot-api-example.git'
+                        url 'https://github.com/Egwonor/book-inventory.git'
                     }
                     branch 'master'
                 }
